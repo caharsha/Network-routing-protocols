5eca4bad4c03b9b1253bb358f18b995a7f0a3072
caharsha <harshacopparam@gmail.com>
2014-03-12 17:59:54 -0500

    Create densegraphgen.cpp

---
+++ densegraphgen.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+#include<ctime>
+using namespace std;
+class densegraph
+{
+	vector <vector <int> > adjmat;
+	int i,j;
+	public:
+	densegraph()
+	{
+		adjmat.resize(5000, vector<int>(5000, -1));
+	}
+	void create(void)
+	{
+		
+		srand (time(NULL));
+		for (i=0;i<5000;i++)
+		{
+			for (j=0;j<5000;j++)
+				{
+					if(i!=j)
+					{
+						if(rand() % 1000 < 106)
+						{
+						adjmat.at(i).at(j) = rand()%32767;
+						adjmat.at(j).at(i) = adjmat.at(i).at(j);
+						}
+					}
+				}
+		}	
+	
+	}
+	void check(void)
+	{
+		double density = 0;
+		int ctr[5000];
+		for (i=0;i<5000;i++)
+		{
+			ctr[i] = 0;
+			for (j=0;j<5000;j++)
+				{
+					if (adjmat.at(i).at(j) != -1)
+					{
+					//cout<<adjmat.at(i).at(j);	
+					ctr[i] ++;
+					density++;
+					}
+				}
+			//cout<<i<<". "<<ctr[i]<<endl;
+		}
+	//cout<<density<<endl;
+	density = density/ (5000*50);
+	cout<<"Percentage of vertices that each vertex is connected to: "<<density<<endl;
+
+	}
+};
+	
+int main()
+{
+	
+	
+	clock_t t1, t2;
+	t1 = clock();
+	densegraph G;
+	G.create();
+	//G.check();
+	t2 = clock();
+	float prog_runtime = float(t2 - t1);
+    prog_runtime = prog_runtime / CLOCKS_PER_SEC;
+    cout<<"The program ran in " <<prog_runtime<<" seconds.";
+return 0;
+}
+
