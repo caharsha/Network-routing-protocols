ce90439a022fd232064aacb96d343b98be261f2f
caharsha <harshacopparam@gmail.com>
2014-03-12 18:00:33 -0500

    Create sparsegraphgen.cpp

---
+++ sparsegraphgen.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+using namespace std;
+class sparsegraph
+{
+	vector <vector <int> > adjmat;	//Vector of vectors
+	int i,j, ctr[5000],temp;
+public:
+sparsegraph()
+{
+	for(i = 0; i < 5000; i++)
+		ctr[i] = 0;
+	adjmat.resize(5000, vector<int>(5000, -1));
+}
+void create(void)
+{
+	srand (time(NULL));
+	for(i = 0;i< 5000; i++)
+	{	
+		for (j = 0; j < 5000; j++)
+		{
+			if(ctr[i] == 6)
+				continue;
+			temp = rand() % 5000;
+			if(i!=temp)
+			if(adjmat.at(i).at(temp) == -1)
+			if(ctr[i] < 6)
+			if(ctr[temp] < 6)
+				{
+					adjmat.at(i).at(temp) = rand() % 32767;
+					adjmat.at(temp).at(i) = adjmat.at(i).at(temp);
+					++ctr[i];
+					++ctr[temp];
+				}
+				
+		}
+	}
+}
+void check(void)
+{
+	for(i = 0; i < 5000; i++)
+	{
+		cout<<endl<<i<<" ";
+		for(j=0;j<5000;j++)
+		{
+			if(adjmat.at(i).at(j) != -1)
+			cout<<j<<" ";
+		}
+	}
+}
+};
+
+int main()
+{
+	sparsegraph G;
+	G.create();
+	G.check();
+	
+		//Matrix 5000 * 5000
+	return 0;
+}
